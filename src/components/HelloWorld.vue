7e780bf04624d49adf52a7ac2c509a6cbddf7001
LuminousHalo <yunhao981@hotmail.com>
2018-09-07 17:35:23 +0800

    QA文字写进数据库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="form-group">
+
+    <el-form>
+      <el-form-item label="问题">
+        <el-input type="textarea" autosize placeholder="随便问点啥问题" v-model="form.questionTextarea"></el-input>
+      </el-form-item>
+      <el-form-item label="回答">
+        <el-input type="textarea" autosize placeholder="这里是回答" v-model="form.answerTextarea"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">提交</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+
+
+
+  <el-upload
+    action="https://jsonplaceholder.typicode.com/posts/"
+    :file-list="fileList">
+    <el-button size="small" type="primary">点击上传</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+  </el-upload>
+
   </div>
 </template>
 
+
+
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
+  data(){
+    return {
+      form: {
+        questionTextarea:'',
+        answerTextarea:'',
+      },
+      fileList:[]
+    }
+  },
   props: {
     msg: String
+  },
+  // async created(){
+  //   let response = await axios.get("http://localhost:3000",{a:"adawdadawd"},{
+  //     header:{
+  //
+  //     }
+  //   })
+  // },
+  methods: {
+    onSubmit(){
+      console.log(this.form.questionTextarea);
+      console.log(this.form.answerTextarea);
+
+      if(this.form.questionTextarea && this.form.answerTextarea){
+        axios.post('http://localhost:3000/question_answer/insert', {
+          question: this.form.questionTextarea,
+          answer: this.form.answerTextarea
+        }).then((res)=>{
+          console.log(res);
+        }).catch((err)=>{
+          console.log(err);
+        });
+      }else{
+        this.openNotification();
+      }
+
+    },
+    openNotification(){
+      const h = this.$createElement;
+      this.$notify({
+        titile: '没有输入完啦',
+        message: h('i', { style: 'color: teal'}, '请填写问题和答案！不要留空')
+      });
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
