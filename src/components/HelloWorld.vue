48f786cf9fa31276310bd435203822f29aaf28fe
LuminousHalo <yunhao981@hotmail.com>
2018-09-09 22:33:05 +0800

    上传按钮调下接口

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,13 @@
 
 
 
+    <el-upload
+            action="http://localhost:3000/question_answer/upload_pictures"
+            :file-list="fileList">
+      <el-button size="small" type="primary">点击上传</el-button>
+      <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+    </el-upload>
+
   <el-upload
     action="https://jsonplaceholder.typicode.com/posts/"
     :file-list="fileList">
